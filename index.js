0dbefb431be155b45ffdfc9da592c50c22976009
Efraim Fernandes <efraim.quintunda@gmail.com>
2023-01-22 00:54:43 -0300

    Adicionando mais capas de filmes

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow,i)=>{
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0
     arrow.addEventListener("click", ()=>{
-        movieLists[i].style.transform = "translateX(-100px)"
+        clickCounter++;
+        if (itemNumber - (6 + clickCounter) >=  0){
+            movieLists[i].style.transform = `translate(${
+                movieLists[i].computedStyleMap().get("transform")[0].x.value
+            -300}px)`;
+        }else{
+            movieLists[i].style.transform = "translate(0)";
+            clickCounter = 0;
+        }
+
     })
+
+    console.log(movieLists[i].querySelectorAll("img").length);
 })
